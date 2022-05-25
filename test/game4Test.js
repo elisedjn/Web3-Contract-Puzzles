88da859fb08c7b2c3708d6e6a282c5b0e8d12a6e
Elise Djn <elisedjn@protonmail.com>
2022-05-25 20:10:44 +0200

    all done

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,16 +1,21 @@
-const { assert } = require("chai");
+const { assert } = require('chai');
 
-describe("Game4", function() {
-  it("should be a winner", async function() {
-    const Game = await ethers.getContractFactory("Game4");
+describe('Game4', function () {
+  it('should be a winner', async function () {
+    const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
     await game.deployed();
 
-    // nested mappings are rough :}
+    const signer1 = ethers.provider.getSigner(0);
+    const address1 = await signer1.getAddress();
 
-    await game.win();
+    const signer2 = ethers.provider.getSigner(1);
+    const address2 = await signer2.getAddress();
+
+    await game.connect(signer1).write(address2);
+    await game.connect(signer2).win(address1);
 
     // leave this assertion as-is
-    assert(await game.isWon(), "You did not win the game");
+    assert(await game.isWon(), 'You did not win the game');
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,16 +1,26 @@
-const { assert } = require("chai");
+const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Game5", function() {
-  it("should be a winner", async function() {
-    const Game = await ethers.getContractFactory("Game5");
+describe('Game5', function () {
+  it('should be a winner', async function () {
+    const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    let wallet = ethers.Wallet.createRandom();
+    while (wallet.address.slice(0, 4) !== '0x00') {
+      wallet = ethers.Wallet.createRandom();
+    }
 
-    await game.win();
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther('1'),
+    });
+
+    await game.connect(wallet.connect(ethers.provider)).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), "You did not win the game");
+    assert(await game.isWon(), 'You did not win the game');
   });
 });
